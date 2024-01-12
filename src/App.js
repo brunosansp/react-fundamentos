ebb23988088cf08a8eaa1f5568908772113fb071
Bruno Santos <brunoadsjp@gmail.com>
2024-01-12 18:59:12 -0300

    create first component

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// O React obriga que todo componente inicie com a primeira letra em maiúsculo
+function App() {
+    return <h1>Component App</h1>
+}
+
+export default App;
